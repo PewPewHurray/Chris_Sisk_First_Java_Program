b61220cca4fe5ac2b6b397f2a2211e0d430fb341
Chris Sisk <dogsrule_2003@yahoo.com>
2022-05-08 17:09:00 -0700

    initial commit

---
+++ FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.printf("My name is Chris Sisk\n");
+        System.out.printf("I am 32 years old\n");
+        System.out.println("My hometown is McCleary, WA");
+    }
+}
